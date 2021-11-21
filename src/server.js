79ac52c94f477cf522dc0c3bb44e5be342997e16
sZeuSz <zeus.k99@gmail.com>
2021-11-20 21:07:17 -0400

    sign-up route finished and tests sign-up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import './setup.js';
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT);
